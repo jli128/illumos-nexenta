eb5155117e5bf58865a7c10a5c95d69765d64cf7
Garrett D'Amore <garrett@nexenta.com>
2011-02-07 20:02:57 -0800

    Import ncp3-gate 3c92e3124504 re #3780 rb322 Spontaneous reboot in igb    bkport-c2b4f6c2d38a-igb-tx-recycle.patch # User Guoqing Zhu <Guoqing.Zhu@Sun.COM> # Date 1280718158 -28800 6935725 igb driver does not follow Kawela spec for setting the device up for external loopback mode 6963343 igb_tx_recycle_head_wb can't distinguish between empty and full of circular descriptor queue 6963350 tx reschedule may never get dispatched when tx ring size is set too small 6966781 system panic during tcptx transferring on igb when disabling tx head wb
       bkport-ebcd04af6c4a-igb-resume-fix.patch
    # User Crisson Guanghao Hu <Crisson.Hu@Sun.COM>
    # Date 1279076944 -28800
    6944756 igb driver does NOT resume correctly on X4270 M2

--- usr/src/uts/common/io/igb/igb_sw.h
+++ usr/src/uts/common/io/igb/igb_sw.h
@@ -160,6 +160,7 @@ extern "C" {
 #define	DEFAULT_TX_RECYCLE_THRESHOLD	(MAX_COOKIE + 1)
 #define	DEFAULT_TX_OVERLOAD_THRESHOLD	MIN_NUM_TX_DESC
 #define	DEFAULT_TX_RESCHED_THRESHOLD	128
+#define	DEFAULT_TX_RESCHED_THRESHOLD_LOW	32
 #define	DEFAULT_MCAST_NUM		4096
 
 #define	IGB_LSO_MAXLEN			65535
@@ -380,6 +381,7 @@ typedef struct dma_buffer {
  */
 typedef struct tx_control_block {
 	single_link_t		link;
+	uint32_t		last_index;
 	uint32_t		frag_num;
 	uint32_t		desc_num;
 	mblk_t			*mp;
