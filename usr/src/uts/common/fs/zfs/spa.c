32ca819ccb37d1f004a726d7014b17ee675571b3
Yuri Pankov <yuri.pankov@nexenta.com>
2013-02-01 23:12:43 +0400

    re #12684 rb4206 importing pool with autoreplace=on and "hole" vdevs crashes syseventd

--- usr/src/uts/common/fs/zfs/spa.c
+++ usr/src/uts/common/fs/zfs/spa.c
@@ -1510,7 +1510,8 @@ spa_check_removed(vdev_t *vd)
 	for (int c = 0; c < vd->vdev_children; c++)
 		spa_check_removed(vd->vdev_child[c]);
 
-	if (vd->vdev_ops->vdev_op_leaf && vdev_is_dead(vd)) {
+	if (vd->vdev_ops->vdev_op_leaf && vdev_is_dead(vd) &&
+	    !vd->vdev_ishole) {
 		zfs_post_autoreplace(vd->vdev_spa, vd);
 		spa_event_notify(vd->vdev_spa, vd, ESC_ZFS_VDEV_CHECK);
 	}
