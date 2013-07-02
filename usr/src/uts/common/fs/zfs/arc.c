88d52f93183d54c2f5f385bca9caffaaf2031d2c
Boris Protopopov <boris.protopopov@nexenta.com>
2013-07-01 17:31:10 -0700

    re #14119 BAD-TRAP panic under load

--- usr/src/uts/common/fs/zfs/arc.c
+++ usr/src/uts/common/fs/zfs/arc.c
@@ -3272,6 +3272,7 @@ arc_release(arc_buf_t *buf, void *tag)
 	if (l2hdr) {
 		mutex_enter(&l2arc_buflist_mtx);
 		hdr->b_l2hdr = NULL;
+		list_remove(l2hdr->b_dev->l2ad_buflist, hdr);
 		buf_size = hdr->b_size;
 	}
 
@@ -3354,7 +3355,6 @@ arc_release(arc_buf_t *buf, void *tag)
 	buf->b_private = NULL;
 
 	if (l2hdr) {
-		list_remove(l2hdr->b_dev->l2ad_buflist, hdr);
 		kmem_free(l2hdr, sizeof (l2arc_buf_hdr_t));
 		ARCSTAT_INCR(arcstat_l2_size, -buf_size);
 		mutex_exit(&l2arc_buflist_mtx);
