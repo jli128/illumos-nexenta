c9353fb33c0728f92814c4c03c70570766031914
Matt Barden <matt.barden@nexenta.com>
2013-12-31 11:52:11 -0500

    SMB-92 Panic with smbtorture raw.samba3oplocklogoff

--- usr/src/uts/common/fs/smbsrv/smb_common_open.c
+++ usr/src/uts/common/fs/smbsrv/smb_common_open.c
@@ -355,7 +355,7 @@ smb_open_subr(smb_request_t *sr)
 	case STYPE_IPC:
 
 		if ((rc = smb_threshold_enter(&sv->sv_opipe_ct)) != 0) {
-			status = RPC_NT_SERVER_TOO_BUSY; /* XXX? */
+			status = RPC_NT_SERVER_TOO_BUSY;
 			return (status);
 		}
 
@@ -773,6 +773,18 @@ smb_open_subr(smb_request_t *sr)
 	    &err);
 	if (of == NULL) {
 		status = err.status;
+	}
+
+	/*
+	 * We might have blocked in smb_ofile_open long enough so a
+	 * tree disconnect might have happened.  In that case, we've
+	 * just added an ofile to a tree that's disconnecting, and
+	 * need to undo that to avoid interfering with tear-down of
+	 * the tree connection.
+	 */
+	if (status == NT_STATUS_SUCCESS &&
+	    !smb_tree_is_connected(sr->tid_tree)) {
+		status = NT_STATUS_INVALID_PARAMETER;
 	}
 
 	/*
