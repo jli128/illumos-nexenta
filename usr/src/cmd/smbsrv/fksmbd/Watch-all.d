03f4468024354f2c228a5e0951628ac465527e8e
Gordon Ross <gwr@nexenta.com>
2014-01-03 18:08:03 -0500

    SMB-95 improve smbsrv developer tools

--- usr/src/cmd/smbsrv/fksmbd/Watch-all.d
+++ usr/src/cmd/smbsrv/fksmbd/Watch-all.d
@@ -11,14 +11,22 @@
  */
 
 /*
- * Copyright 2013 Nexenta Systems, Inc.  All rights reserved.
+ * Copyright 2014 Nexenta Systems, Inc.  All rights reserved.
  */
 
 /*
- * User-level dtrace for smbd
- * Usage: dtrace -s ThisScript.d -p PID
+ * User-level dtrace for fksmbd
+ * Usage: dtrace -s Watch-all.d -p $PID
  */
 
+#pragma D option flowindent
+
+self int trace;
+self int mask;
+
+/*
+ * Trace almost everything
+ */
 pid$target:fksmbd::entry,
 pid$target:libfksmbsrv.so.1::entry,
 pid$target:libmlsvc.so.1::entry,
@@ -26,10 +34,64 @@ pid$target:libmlrpc.so.1::entry,
 pid$target:libsmbns.so.1::entry,
 pid$target:libsmb.so.1::entry
 {
+  self->trace++;
+}
+
+/*
+ * If traced and not masked, print entry/return
+ */
+pid$target:fksmbd::entry,
+pid$target:libfksmbsrv.so.1::entry,
+pid$target:libmlsvc.so.1::entry,
+pid$target:libmlrpc.so.1::entry,
+pid$target:libsmbns.so.1::entry,
+pid$target:libsmb.so.1::entry
+/self->trace > 0 && self->mask == 0/
+{
   printf("\t0x%x", arg0);
   printf("\t0x%x", arg1);
   printf("\t0x%x", arg2);
   printf("\t0x%x", arg3);
+}
+
+/*
+ * Mask (don't print) all function calls below these functions.
+ * These make many boring, repetitive function calls like
+ * smb_mbtowc, mbc_marshal_...
+ */
+pid$target::fop__getxvattr:entry,
+pid$target::smb_mbc_vdecodef:entry,
+pid$target::smb_mbc_vencodef:entry,
+pid$target::smb_msgbuf_decode:entry,
+pid$target::smb_msgbuf_encode:entry,
+pid$target::smb_strlwr:entry,
+pid$target::smb_strupr:entry,
+pid$target::smb_wcequiv_strlen:entry
+{
+  self->mask++;
+}
+
+pid$target::fop__getxvattr:return,
+pid$target::smb_mbc_vdecodef:return,
+pid$target::smb_mbc_vencodef:return,
+pid$target::smb_msgbuf_decode:return,
+pid$target::smb_msgbuf_encode:return,
+pid$target::smb_strlwr:return,
+pid$target::smb_strupr:return,
+pid$target::smb_wcequiv_strlen:return
+{
+  self->mask--;
+}
+
+pid$target:fksmbd::return,
+pid$target:libfksmbsrv.so.1::return,
+pid$target:libmlsvc.so.1::return,
+pid$target:libmlrpc.so.1::return,
+pid$target:libsmbns.so.1::return,
+pid$target:libsmb.so.1::return
+/self->trace > 0 && self->mask == 0/
+{
+  printf("\t0x%x", arg1);
 }
 
 pid$target:fksmbd::return,
@@ -39,5 +101,5 @@ pid$target:libmlrpc.so.1::return,
 pid$target:libsmbns.so.1::return,
 pid$target:libsmb.so.1::return
 {
-  printf("\t0x%x", arg1);
+  self->trace--;
 }
